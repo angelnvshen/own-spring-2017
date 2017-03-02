3db3744f53483d4317561f67e90398e6b9f623f3
angelnvshen <angelnvshen@163.com>
2017-03-02 13:13:51 +0800

    this new spring project

---
+++ src/main/java/own/stu/spring/OwnSpringApplication.java
@@ -0,0 +1,12 @@
+package own.stu.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OwnSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OwnSpringApplication.class, args);
+	}
+}
