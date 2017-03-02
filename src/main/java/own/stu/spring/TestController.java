f29a7a79092f8068ca8ef224e41fcff3e72ba6bc
angelnvshen <angelnvshen@163.com>
2017-03-02 13:31:41 +0800

    add sth for base spring boot

---
+++ src/main/java/own/stu/spring/TestController.java
@@ -0,0 +1,19 @@
+package own.stu.spring;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Created by dell on 2017/3/2.
+ */
+@Controller
+@RequestMapping("test")
+public class TestController {
+
+    @ResponseBody
+    @RequestMapping
+    public String test(){
+        return "success";
+    }
+}
