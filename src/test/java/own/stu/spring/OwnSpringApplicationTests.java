eef0e1e246217f010926f2b9a3202391049294d0
angelnvshen <angelnvshen@163.com>
2017-03-12 15:31:36 +0800

    spring - log4j2 with jdbc basic config

--- src/test/java/own/stu/spring/OwnSpringApplicationTests.java
+++ src/test/java/own/stu/spring/OwnSpringApplicationTests.java
@@ -2,15 +2,25 @@ package own.stu.spring;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class OwnSpringApplicationTests {
+    Logger logger = LoggerFactory.getLogger(getClass());
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+    }
+
+    @Test
+    public void test() {
+        logger.info(" =============== hello =============== ");
+        logger.warn(" =============== hello =============== ");
+        logger.error(" =============== hello =============== ");
+    }
 
 }
