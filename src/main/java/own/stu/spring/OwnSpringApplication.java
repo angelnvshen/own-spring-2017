cd1380a3ef73f7bc1068f6b57ffd0ca3a7a1809d
angelnvshen <angelnvshen@163.com>
2017-03-22 11:04:54 +0800

    ......

--- src/main/java/own/stu/spring/OwnSpringApplication.java
+++ src/main/java/own/stu/spring/OwnSpringApplication.java
@@ -2,11 +2,19 @@ package own.stu.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class OwnSpringApplication {
+public class OwnSpringApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(OwnSpringApplication.class);
+	}
+
+	public static void main(String[] args) throws Exception {
 		SpringApplication.run(OwnSpringApplication.class, args);
 	}
+
 }
